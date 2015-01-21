73475498e42a1a87afc149e1afb7ff6ae91a3748
etudiant <etudiant@g141-debian>
2015-01-21 11:59:25 +0100

    premier truc

---
+++ main.c
@@ -0,0 +1,12 @@
+double operandeA;
+double operandeB;
+double resultat;
+
+operandeA 17;
+operandeB 4;
+
+resultat = operandeA * operandeB;
+return resultat;
+
+resultat = operandeA / operandeB;
+return resultat;
